628cfdba92d2bd745885626cd9b941991b189467
amasheshadee <53006150+amasheshadee@users.noreply.github.com>
2020-11-17 17:20:18 +0530

    Navitation to another page

--- app/src/main/java/com/example/firstactivity/MainActivity.java
+++ app/src/main/java/com/example/firstactivity/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.firstactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        OnclickButtonListner();
+
     }
+
+    public void OnclickButtonListner(){
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+
 }
