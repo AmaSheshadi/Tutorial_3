e88adb7339579ac2ae40b6e9ee4b69e284527db5
amasheshadee <53006150+amasheshadee@users.noreply.github.com>
2020-11-18 17:52:34 +0530

    calculator

--- app/src/main/java/com/example/firstactivity/MainActivity.java
+++ app/src/main/java/com/example/firstactivity/MainActivity.java
@@ -6,23 +6,63 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
-    Toast toast;
+    //Toast toast;
+    EditText editText1;
+    EditText editText2;
+
+    TextView lblSum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        OnclickButtonListner();
+
+        //Define the objects
+        editText1 = findViewById(R.id.editText1);
+        editText2 = findViewById(R.id.editText2);
+        lblSum = findViewById(R.id.lblSum);
+        //OnclickButtonListner();
 
     }
+    public void openSecond(View view){
+        Intent intent = new Intent(this,SecondActivity.class);
+        intent.putExtra("n1",editText1.getText().toString());
+        intent.putExtra("n2",editText2.getText().toString());
 
-    public void OnclickButtonListner(){
+        //Toast.makeText(this,"Opening Calculator..." , Toast.LENGTH_SHORT).show();
+
+        //Creating the LayoutInflater instanc
+
+        LayoutInflater li = getLayoutInflater();
+        //Getting the View object as defined in the customtoast.xml file
+        View layout = li.inflate(R.layout.customtoast, (ViewGroup)
+                findViewById(R.id.custom_toast_layout));
+        //Creating the Toast object
+        Toast toast = new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.setView(layout);//setting the view of custom toast layout
+        toast.show();
+
+        startActivity(intent);
+    }
+
+
+
+
+
+
+    /*public void OnclickButtonListner(){
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(
                 new View.OnClickListener() {
@@ -35,11 +75,13 @@ public class MainActivity extends AppCompatActivity {
                         toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0 );
                         toast.show();
 
-                        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                        startActivity(intent);
+                        //Toast.makeText(this,"Opening Calculator..." , Toast.LENGTH_SHORT).show();
+
+                       //Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                       // startActivity(intent);
                     }
                 }
         );
-    }
+    }*/
 
 }
--- app/src/main/java/com/example/firstactivity/SecondActivity.java
+++ app/src/main/java/com/example/firstactivity/SecondActivity.java
@@ -2,13 +2,56 @@ package com.example.firstactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    EditText editText1;
+    EditText editText2;
+
+    String number1;
+    String number2;
+
+    TextView lblSum;
+
+    int n1,n2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        editText1 = findViewById(R.id.editText1);
+        editText2 = findViewById(R.id.editText2);
+
+        Intent intent = getIntent();
+
+        String number1 = intent.getStringExtra("n1");
+        String number2 = intent.getStringExtra("n2");
+
+        editText1.setText(number1);
+        editText2.setText(number2);
+
+        n1 = Integer.parseInt(number1);
+        n2 = Integer.parseInt(number2);
+
+    }
+
+    public void add(View view){
+        lblSum.setText(number1 + " + " + number2 + " = " + (n1+n2));
+    }
+
+    public void subtract(View view){
+        lblSum.setText(number1 + " - " + number2 + " = " + (n1-n2));
+    }
+
+    public void divide(View view){
+        lblSum.setText(number1 + " / " + number2 + " = " + (n1/n2));
+    }
+
+    public void multiply(View view){
+        lblSum.setText(number1 + " * " + number2 + " = " + (n1*n2));
     }
 }
